e6827691b8fe7a362287e6d6cd16b95735e2b82e
llblch <mustqwert@gmail.com>
2022-09-09 07:44:52 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- *  * main - entry point
- *   * Return: 0
- *    */
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
