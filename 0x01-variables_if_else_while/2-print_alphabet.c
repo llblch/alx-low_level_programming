1f5696c8274ff842a28545fe12cae2c1ec1d7287
llblch <mustqwert@gmail.com>
2022-09-12 08:30:50 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+include <time.h>
+/**
+ * main - a program that prints the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	printf("\n");
+	return (0)
+}
