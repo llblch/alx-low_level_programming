b3f935b87743b2a70ef892ec3ca246abc6b74694
llblch <mustqwert@gmail.com>
2022-09-12 20:06:43 -0700

    102-print_comb5.c

---
+++ 102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stduio.h>
+/**
+ *main - print possible combination of two 2 digit numbers
+ *
+ * Return:returns zero at end
+ */
+int main(void)
+{
+	int i, p;
+
+	for (i = 0; i < 100; j++)
+	{
+		if (p > i)
+		{
+			putchar((i / 10) + '0');
+			Putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
