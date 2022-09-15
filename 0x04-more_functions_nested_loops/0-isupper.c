fc6649b5a84803581639c0afdf18b95acd103a54
llblch <mustqwert@gmail.com>
2022-09-15 00:42:38 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase.
+ * @c: character to check.
+ * Return: 1 if c is uppercase, else return 0
+ */
+int _isupper(int c)
+{
+	f(c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
