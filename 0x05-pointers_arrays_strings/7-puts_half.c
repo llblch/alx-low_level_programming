da7aece753cc9cf1d0f452b9e89666fca8096b90
llblch <mustqwert@gmail.com>
2022-09-20 05:21:13 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,18 +10,17 @@
 
 void puts_half(char *str)
 {
-	int i, j;
+	int index = 0, len = 0, n;
 
-	while (str[i] != '\0')
-		i++;
-	if (i % 2 == 0)
-		j = i / 2;
+	while (str[index++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
 	else
-		j = (i + 1) / 2;
-	while (j < i)
-	{
-		putchar(str[j]);
-		j++;
-	}
+		n = (len + 1) / 2;
+	for (index = n; index < len; index++)
+		putchar(str[index]);
 	putchar('\n');
 }
