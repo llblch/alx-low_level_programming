88a2c922ce23bbd8b21284591ac9f279f57c1cf2
llblch <mustqwert@gmail.com>
2022-09-20 05:46:32 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,5 +10,18 @@
 
 int main(void)
 {
+	int myrand;
+	int count;
+	int total;
+
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
+
 	return (0);
 }
