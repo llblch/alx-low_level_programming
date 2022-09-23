b6275121715883ad6603d17ca4f15d7d5ce2ba37
llblch <mustqwert@gmail.com>
2022-09-23 09:30:24 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,10 +2,15 @@
 #define MAIN_H
 #include <stdio.h>
 /**
- * char prototypes()
- * int prototypes()
  * void prototypes()
+ * int prototypes()
+ * char prototypes()
  */
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+int _strcmp(char *s1, char *s2);
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -15,10 +20,5 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
-int _strcmp(char *s1, char *s2);
-int _putchar(char);
-void reverse_array(int *a, int n);
-void print_number(int n);
-void print_buffer(char *b, int size);
 
 #endif /*MAIN_H*/
