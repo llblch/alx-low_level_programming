c87a85748b64d077188a0c4192851853ef70a88b
llblch <mustqwert@gmail.com>
2022-12-15 22:51:59 -0800

    C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character to check for
+ *
+ * pointer to spot in s with c or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return ('\0');
+}
